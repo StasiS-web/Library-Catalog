cc3504fc61c24f765bc6535928bf6c773360d378
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2023-11-22 17:00:58 +0000

    add verification test for login and register buttons

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,10 +1,28 @@
 const { test, expect } = require('@playwright/test');
+const pageUrl = 'http://localhost:3001';
 
 test('Verify "All Books" link is visible', async ({ page }) => {
-    await page.goto('http://localhost:3001');
+    await page.goto(pageUrl);
     await page.waitForSelector('nav.navbar');
 
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
     expect(isLinkVisible).toBe(true);
 });
+
+test('Verify "Login" button is visible', async ({ page }) => {
+    await page.goto(pageUrl);
+    await page.waitForSelector('nav.navbar');
+    const loginButton = await page.$('a[href="/login"]');
+    const isLoginButtonVisible = await loginButton.isVisible();
+    expect(isLoginButtonVisible).toBe(true);
+});
+
+test('Verify "Register" button is visible', async ({ page }) => {
+    await page.goto(pageUrl);
+    await page.waitForSelector('nav.navbar');
+    const registerButton = await page.$('a[href="/register"]');
+    const isRegisterButtonVisible = await registerButton.isVisible();
+    expect(isRegisterButtonVisible).toBe(true);
+});
+
